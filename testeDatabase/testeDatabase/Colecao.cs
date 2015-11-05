797141802912a6321e86d11a0097a7ed284d0d15
unknown <15105188@W1846971.portoalegre.pucrsnet.br>
2015-11-05 18:19:48 -0200

    dfj

---
+++ testeDatabase/testeDatabase/Colecao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeDatabase
+{
+    class Colecao : INotifyPropertyChanged
+    {
+        private List<String> vet;
+        public Colecao()
+        {
+            vet = new List<String>();
+            vet.Add("casa");
+            vet.Add("carro");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ testeDatabase/testeDatabase/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testeDatabase
+{
+    public partial class Form1 : Form
+    {
+        private String nome;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nome = textBox1.Text;
+            textBox2.Text = nome;
+            listBox1.Items.Add(" jkasdka");
+        }
+    }
+}
